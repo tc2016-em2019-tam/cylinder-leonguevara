c67ec5340e30f90ac39ece5b1cf336b8ad736a24
Leon Felipe Guevara Chavez <leon.guevara@itesm.mx>
2019-02-21 13:45:13 -0600

    Initial Commit

---
+++ src/com/lguevara/Circle.java
@@ -0,0 +1,49 @@
+package com.lguevara;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" +
+                "radius=" + radius +
+                ",color='" + color + '\'' +
+                ']';
+    }
+}
